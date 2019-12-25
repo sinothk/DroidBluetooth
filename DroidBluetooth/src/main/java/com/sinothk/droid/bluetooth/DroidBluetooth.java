92aeca0c36ae7128ef3f6ddc4101efb1e3bedc11
LiangYT <381518188@qq.com>
2019-12-25 09:39:21 +0800

    初始化！

---
+++ DroidBluetooth/src/main/java/com/sinothk/droid/bluetooth/DroidBluetooth.java
@@ -0,0 +1,50 @@
+package com.sinothk.droid.bluetooth;
+
+import android.bluetooth.BluetoothAdapter;
+import android.content.Context;
+
+public class DroidBluetooth {
+
+    private static BluetoothAdapter blueadapter;
+    private static boolean support;
+
+    /**
+     * 初始化
+     *
+     * @param baseContext
+     */
+    public static void init(Context baseContext) {
+        //获取蓝牙适配器
+        blueadapter = BluetoothAdapter.getDefaultAdapter();
+        //表示手机支持蓝牙
+        support = blueadapter != null;
+    }
+
+    /**
+     * 判断设备是否支持蓝牙
+     *
+     * @return
+     */
+    public static boolean isSupport() {
+        return support;
+    }
+
+    /**
+     * 打开蓝牙
+     */
+    public static void open() {
+        //判断本机蓝牙是否打开
+        if (!blueadapter.isEnabled()) {//如果没打开，则打开蓝牙
+            blueadapter.enable();
+        }
+    }
+
+    /**
+     * 判断蓝牙是否打开
+     *
+     * @return
+     */
+    public static boolean isEnable() {
+        return blueadapter.isEnabled();
+    }
+}
--- app/src/main/java/com/sinothk/droid/bluetooth/demo/MainActivity.java
+++ app/src/main/java/com/sinothk/droid/bluetooth/demo/MainActivity.java
@@ -2,7 +2,11 @@ package com.sinothk.droid.bluetooth.demo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothManager;
 import android.os.Bundle;
+
+import com.sinothk.droid.bluetooth.DroidBluetooth;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +15,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        DroidBluetooth.init(getBaseContext());
+
+        boolean isSupport = DroidBluetooth.isSupport();
+        boolean isEnable = DroidBluetooth.isEnable();
 
     }
 }
