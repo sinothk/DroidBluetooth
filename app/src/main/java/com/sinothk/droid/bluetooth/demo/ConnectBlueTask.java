b67cfc5b57f7a3d883516419e981c0c035b2960b
LiangYT <381518188@qq.com>
2019-12-25 15:15:09 +0800

    初始化！

--- app/src/main/java/com/sinothk/droid/bluetooth/demo/ConnectBlueTask.java
+++ app/src/main/java/com/sinothk/droid/bluetooth/demo/ConnectBlueTask.java
@@ -6,59 +6,73 @@ import android.os.AsyncTask;
 import android.util.Log;
 
 import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.UUID;
 
-/**连接线程
+/**
+ * 连接线程
  * Created by zqf on 2018/7/7.
  */
 
 public class ConnectBlueTask extends AsyncTask<BluetoothDevice, Integer, BluetoothSocket> {
     private static final String TAG = ConnectBlueTask.class.getName();
-    private BluetoothDevice bluetoothDevice;
+    private BluetoothDevice device;
     private ConnectBlueCallBack callBack;
 
-    public ConnectBlueTask(ConnectBlueCallBack callBack){
+    public ConnectBlueTask(ConnectBlueCallBack callBack) {
         this.callBack = callBack;
     }
 
     @Override
     protected BluetoothSocket doInBackground(BluetoothDevice... bluetoothDevices) {
-        bluetoothDevice = bluetoothDevices[0];
+        device = bluetoothDevices[0];
         BluetoothSocket socket = null;
-        try{
+//        try{
+//
+//            String uuid = "00001101-0000-1000-8000-00805F9B34FB";//.toString().replaceAll("-","");
+//            Log.d(TAG,"开始连接socket, uuid:" + uuid);
+//            socket = bluetoothDevice.createRfcommSocketToServiceRecord(UUID.fromString(uuid));
+//            if (socket != null && !socket.isConnected()){
+//                socket.connect();
+//            }
+//        }catch (IOException e){
+//            Log.e(TAG,"socket连接失败");
+//            try {
+//                socket.close();
+//            } catch (IOException e1) {
+//                e1.printStackTrace();
+//                Log.e(TAG,"socket关闭失败");
+//            }
+//        }
+//        return socket;
+        try {
+            socket = (BluetoothSocket) device.getClass().getDeclaredMethod("createRfcommSocket", new Class[]{int.class}).invoke(device, 1);
 
-            String uuid = "00001101-0000-1000-8000-00805F9B34FB";//.toString().replaceAll("-","");
-            Log.d(TAG,"开始连接socket, uuid:" + uuid);
-            socket = bluetoothDevice.createRfcommSocketToServiceRecord(UUID.fromString(uuid));
-            if (socket != null && !socket.isConnected()){
+            if (socket != null && !socket.isConnected()) {
                 socket.connect();
+                return socket;
             }
-        }catch (IOException e){
-            Log.e(TAG,"socket连接失败");
-            try {
-                socket.close();
-            } catch (IOException e1) {
-                e1.printStackTrace();
-                Log.e(TAG,"socket关闭失败");
-            }
+        } catch (IllegalAccessException | InvocationTargetException | NoSuchMethodException | IOException e) {
+            e.printStackTrace();
         }
-        return socket;
+        return null;
     }
 
     @Override
     protected void onPreExecute() {
-        Log.d(TAG,"开始连接");
+        Log.d(TAG, "开始连接");
         if (callBack != null) callBack.onStartConnect();
     }
 
     @Override
     protected void onPostExecute(BluetoothSocket bluetoothSocket) {
-        if (bluetoothSocket != null && bluetoothSocket.isConnected()){
-            Log.d(TAG,"连接成功");
-            if (callBack != null) callBack.onConnectSuccess(bluetoothDevice, bluetoothSocket);
-        }else {
-            Log.d(TAG,"连接失败");
-            if (callBack != null) callBack.onConnectFail(bluetoothDevice, "连接失败");
+        if (bluetoothSocket != null && bluetoothSocket.isConnected()) {
+            Log.d(TAG, "连接成功");
+            if (callBack != null) callBack.onConnectSuccess(device, bluetoothSocket);
+        } else {
+            Log.d(TAG, "连接失败");
+            if (callBack != null) callBack.onConnectFail(device, "连接失败");
         }
     }
 }
